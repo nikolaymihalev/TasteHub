b1650ea8990348a45600e978995ab65bb537b4f7
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-23 10:43:32 +0000

    Create project with data and service layers

---
+++ TasteHub/TasteHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TasteHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
