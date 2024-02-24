752835d801d02545338041ef3ca73168934f984f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-24 17:32:28 +0000

    Add validation and comments to recipe entity

--- TasteHub/TasteHub.Infrastructure/Data/Constants/ValidationConstants.cs
+++ TasteHub/TasteHub.Infrastructure/Data/Constants/ValidationConstants.cs
@@ -4,6 +4,14 @@
     {
         public const int CategoryNameMaxLength = 60;
         public const int CategoryNameMinLength = 3;
-
+        
+        public const int RecipeTitleMaxLength = 100;
+        public const int RecipeTitleMinLength = 4;
+        
+        public const int RecipeIngredientsMaxLength = 300;
+        public const int RecipeIngredientsMinLength = 5;
+        
+        public const int RecipeInstructionsMaxLength = 1500;
+        public const int RecipeInstructionsMinLength = 10;
     }
 }
--- TasteHub/TasteHub.Infrastructure/Data/Models/Recipe.cs
+++ TasteHub/TasteHub.Infrastructure/Data/Models/Recipe.cs
@@ -1,39 +1,48 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TasteHub.Infrastructure.Data.Constants;
 
 namespace TasteHub.Infrastructure.Data.Models
 {
+    [Comment("Recipe entity")]
     public class Recipe
     {
+        [Comment("Recipe identifier")]
         [Key]
         public int Id { get; set; }
 
+        [Comment("Recipe title")]
         [Required]
+        [MaxLength(ValidationConstants.RecipeTitleMaxLength)]
         public string Title { get; set; } = string.Empty;
-        
+
+        [Comment("Recipe description")]
         public string Description { get; set; } = string.Empty;
 
+        [Comment("Recipe ingredients")]
         [Required]
+        [MaxLength(ValidationConstants.RecipeIngredientsMaxLength)]
         public string Ingredients { get; set; } = string.Empty;
 
+        [Comment("Recipe instructions")]
         [Required]
+        [MaxLength(ValidationConstants.RecipeInstructionsMaxLength)]
         public string Instructions { get; set; } = string.Empty;
 
+        [Comment("Date the recipe was created")]
         [Required]
         public DateTime CreationDate { get; set; }
 
+        [Comment("Recipe creator")]
         [Required]
         public string CreatorId { get; set; }
 
         [ForeignKey(nameof(CreatorId))]
         public IdentityUser Creator { get; set; }
 
+        [Comment("Recipe category identifier")]
         [Required]
         public int CategoryId { get; set; }
 
