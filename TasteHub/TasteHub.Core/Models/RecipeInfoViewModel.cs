d989140dc86faedece34ff216f7798e5411b6fbf
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-27 13:13:22 +0000

    Created Recipe info model

--- TasteHub/TasteHub.Core/Models/RecipeInfoViewModel.cs
+++ TasteHub/TasteHub.Core/Models/RecipeInfoViewModel.cs
@@ -1,12 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TasteHub.Core.Models
+﻿namespace TasteHub.Core.Models
 {
+    /// <summary>
+    /// Model for recipe information in a database
+    /// </summary>
     public class RecipeInfoViewModel
     {
+        public RecipeInfoViewModel(
+            int id,
+            string title,
+            string? description,
+            string ingredients,
+            string instructions,
+            string creationDate,
+            byte[] image,
+            string creatorId,
+            int categoryId)
+        {
+            Id = id; 
+            Title = title; 
+            Description = description;
+            Ingredients = ingredients;
+            Instructions = instructions;
+            CreationDate = creationDate;
+            Image = image;
+            CreatorId = creatorId;
+            CategoryId = categoryId;
+        }
+
+        /// <summary>
+        /// Recipe identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Recipe title
+        /// </summary>
+        public string Title { get; set; } 
+
+        /// <summary>
+        /// Recipe description
+        /// </summary>
+        public string? Description { get; set; } 
+
+        /// <summary>
+        /// Recipe ingredients
+        /// </summary>
+        public string Ingredients { get; set; } 
+        
+        /// <summary>
+        /// Instructions for making the recipe
+        /// </summary>
+        public string Instructions { get; set; }
+
+        /// <summary>
+        /// Date of creation
+        /// </summary>
+        public string CreationDate { get; set; }
+
+        /// <summary>
+        /// Image of the food
+        /// </summary>
+        public byte[] Image { get; set; }
+        
+        /// <summary>
+        /// Creator identifier
+        /// </summary>
+        public string CreatorId { get; set; } 
+
+        /// <summary>
+        /// Category identifier
+        /// </summary>
+        public int CategoryId { get; set; }
     }
 }
