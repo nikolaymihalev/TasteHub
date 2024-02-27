01461b2833eb1cd1e1a5e6aa545b80a4f4230c6d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-27 13:22:26 +0000

    Created recipe form model

--- TasteHub/TasteHub.Core/Models/RecipeFormViewModel.cs
+++ TasteHub/TasteHub.Core/Models/RecipeFormViewModel.cs
@@ -1,12 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using TasteHub.Infrastructure.Constants;
+using TasteHub.Infrastructure.Data.Models;
 
 namespace TasteHub.Core.Models
 {
+    /// <summary>
+    /// Model for adding or edditing recipe
+    /// </summary>
     public class RecipeFormViewModel
-    {
+    {        
+        /// <summary>
+        /// Recipe identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Recipe title
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.RecipeTitleMaxLength, 
+            MinimumLength = ValidationConstants.RecipeTitleMinLength, 
+            ErrorMessage = ErrorMessageConstants.StringLengthErrorMessage)]
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Recipe description
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Recipe ingredients
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.RecipeIngredientsMaxLength,
+            MinimumLength = ValidationConstants.RecipeIngredientsMinLength,
+            ErrorMessage = ErrorMessageConstants.StringLengthErrorMessage)]
+        public string Ingredients { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Instructions for making the recipe
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.RecipeInstructionsMaxLength,
+             MinimumLength = ValidationConstants.RecipeInstructionsMinLength,
+             ErrorMessage = ErrorMessageConstants.StringLengthErrorMessage)]
+        public string Instructions { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Date of creation
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        //Binding format needed
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Image of the food
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
+        public byte[] Image { get; set; }
+
+        /// <summary>
+        /// Category identifier
+        /// </summary>
+        public int? CategoryId { get; set; }
+
+        // Collection of categories needed
     }
 }
--- TasteHub/TasteHub.Infrastructure/Constants/ErrorMessageConstants.cs
+++ TasteHub/TasteHub.Infrastructure/Constants/ErrorMessageConstants.cs
@@ -8,5 +8,7 @@ namespace TasteHub.Infrastructure.Constants
 {
     public static class ErrorMessageConstants
     {
+        public const string RequireErrorMessage = "The field {0} is required!";
+        public const string StringLengthErrorMessage = "The field {0} is between {2} and {1} characters long!";
     }
 }
