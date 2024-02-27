4dfcbf5ceeff3fb8d176ed6c95ee5365ae1b13fb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-27 13:05:16 +0000

    Created interface for recipe service

---
+++ TasteHub/TasteHub.Core/Contracts/IRecipeService.cs
@@ -0,0 +1,12 @@
+﻿using TasteHub.Core.Models;
+
+namespace TasteHub.Core.Contracts
+{
+    public interface IRecipeService
+    {
+        Task<IEnumerable<RecipeInfoViewModel>> GetAllRecipesAsync();
+        Task AddAsync(RecipeFormViewModel model);
+        Task EditAsync(RecipeFormViewModel model);
+        Task DeleteAsync(int id);
+    }
+}
