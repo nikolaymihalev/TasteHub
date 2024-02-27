0df409399405772dcf9b1d5383f497a6315a8440
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-27 12:30:43 +0000

    Created Service extension class

---
+++ TasteHub/TasteHub/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            return services;
+        }
+        
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            return services;
+        }
+    }
+}
