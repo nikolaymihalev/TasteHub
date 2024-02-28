ce46cb27abf058804c266e8fe18f0d44eeb9263e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-28 17:27:10 +0000

    Created Category service with Get all method

---
+++ TasteHub/TasteHub.Core/Services/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TasteHub.Core.Contracts;
+using TasteHub.Core.Models;
+using TasteHub.Infrastructure.Data;
+
+namespace TasteHub.Core.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ILogger logger;
+
+        public CategoryService(
+            ApplicationDbContext _context,
+            ILogger _logger)
+        {
+            context = _context;
+            logger = _logger;
+        }
+
+        public Task AddAsync(CategoryFormViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(CategoryFormViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<CategoryInfoViewModel>> GetAllCategoriesAsync()
+        {
+            return await context.Categories
+                .AsNoTracking()
+                .Select(x=> new CategoryInfoViewModel(
+                    x.Id,
+                    x.Name))
+                .ToListAsync();
+        }
+    }
+}
