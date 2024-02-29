8b5c840bc330f6bb185898a758017de0be4dacda
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-29 16:11:17 +0000

    Created repository interface

---
+++ TasteHub/TasteHub.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace TasteHub.Infrastructure.Common
+{
+    public interface IRepository : IDisposable
+    {
+        IQueryable<T> AllAsync<T>() where T : class;
+        IQueryable<T> AllAsync<T>(Expression<Func<T,bool>> search) where T : class;          
+        IQueryable<T> AllReadonlyAsync<T>() where T : class;
+        IQueryable<T> AllReadonlyAsync<T>(Expression<Func<T,bool>> search) where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+        Task Delete<T>(T entity) where T : class;
+        Task DeleteRange<T>(IEnumerable<T> entities) where T : class;
+        Task<int> SaveChangesAsync();
+    }
+}
