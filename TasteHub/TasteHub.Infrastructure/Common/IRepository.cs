9ec31785248a3610e79a66619e51ee8dd565f3f2
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-29 16:38:35 +0000

    Added summary

--- TasteHub/TasteHub.Infrastructure/Common/IRepository.cs
+++ TasteHub/TasteHub.Infrastructure/Common/IRepository.cs
@@ -2,17 +2,59 @@
 
 namespace TasteHub.Infrastructure.Common
 {
+    /// <summary>
+    /// Interface for repository methods
+    /// </summary>
     public interface IRepository : IDisposable
     {
+        /// <summary>
+        /// All data in a table
+        /// </summary>
         IQueryable<T> All<T>() where T : class;
-        IQueryable<T> All<T>(Expression<Func<T,bool>> search) where T : class;          
+
+        /// <summary>
+        /// All data in a table
+        /// </summary>
+        IQueryable<T> All<T>(Expression<Func<T,bool>> search) where T : class;   
+        
+        /// <summary>
+        /// All data as no tracking in a table
+        /// </summary>
         IQueryable<T> AllReadonly<T>() where T : class;
+
+        /// <summary>
+        /// All data as no tracking in a table
+        /// </summary>
         IQueryable<T> AllReadonly<T>(Expression<Func<T,bool>> search) where T : class;
+
+        /// <summary>
+        /// Add entity to the database
+        /// </summary>
         Task AddAsync<T>(T entity) where T : class;
+
+        /// <summary>
+        /// Get specific entity from database by identifier
+        /// </summary>
         Task<T?> GetByIdAsync<T>(object id) where T : class;
+        
+        /// <summary>
+        /// Delete entity from database
+        /// </summary>
         Task DeleteAsync<T>(object id) where T : class;
+
+        /// <summary>
+        /// Delete entity from database
+        /// </summary>
         void Delete<T>(T entity) where T : class;
+
+        /// <summary>
+        /// Delete range of entities from database
+        /// </summary>
         void DeleteRange<T>(IEnumerable<T> entities) where T : class;
-        Task<int> SaveChangesAsync();
+
+        /// <summary>
+        /// Save changes in database
+        /// </summary>
+        Task<int> SaveChangesAsync();        
     }
 }
--- TasteHub/TasteHub.Infrastructure/Common/Repository.cs
+++ TasteHub/TasteHub.Infrastructure/Common/Repository.cs
@@ -5,10 +5,19 @@ using TasteHub.Infrastructure.Data;
 
 namespace TasteHub.Infrastructure.Common
 {
+    /// <summary>
+    /// Repository class with methods for Relational Database Engine
+    /// </summary>
     public class Repository : IRepository
     {
+        /// <summary>
+        /// Entity framework Db context
+        /// </summary>
         protected DbContext Context { get; set; }
 
+        /// <summary>
+        /// Table in database
+        /// </summary>
         protected DbSet<T> DbSet<T>() where T : class
         {
             return this.Context.Set<T>();
@@ -19,36 +28,57 @@ namespace TasteHub.Infrastructure.Common
             Context = _context;
         }
 
+        /// <summary>
+        /// All data in a table
+        /// </summary>
         public IQueryable<T> All<T>() where T : class
         {
             return this.DbSet<T>().AsQueryable();
         }
 
+        /// <summary>
+        /// All data in a table
+        /// </summary>
         public IQueryable<T> All<T>(Expression<Func<T, bool>> search) where T : class
         {
             return this.DbSet<T>().Where(search).AsQueryable();
         }
 
+        /// <summary>
+        /// All data as no tracking in a table
+        /// </summary>
         public IQueryable<T> AllReadonly<T>() where T : class
         {
             return this.DbSet<T>().AsNoTracking();
         }
 
+        /// <summary>
+        /// All data as no tracking in a table
+        /// </summary>
         public IQueryable<T> AllReadonly<T>(Expression<Func<T, bool>> search) where T : class
         {
             return this.DbSet<T>().Where(search).AsNoTracking();
         }
 
+        /// <summary>
+        /// Add entity to the database
+        /// </summary>
         public async Task AddAsync<T>(T entity) where T : class
         {
             await this.DbSet<T>().AddAsync(entity);
         }
 
+        /// <summary>
+        /// Get specific entity from database by identifier
+        /// </summary>
         public async Task<T?> GetByIdAsync<T>(object id) where T : class
         {
             return await DbSet<T>().FindAsync(id);
         }
 
+        /// <summary>
+        /// Delete entity from database
+        /// </summary>
         public async Task DeleteAsync<T>(object id) where T : class
         {
             T? entity = await GetByIdAsync<T>(id);
@@ -57,6 +87,9 @@ namespace TasteHub.Infrastructure.Common
                 Delete<T>(entity);
         }
 
+        /// <summary>
+        /// Delete entity from database
+        /// </summary>
         public void Delete<T>(T entity) where T : class
         {
             EntityEntry entry = this.Context.Entry(entity);
@@ -69,16 +102,25 @@ namespace TasteHub.Infrastructure.Common
             entry.State = EntityState.Deleted;
         }
 
+        /// <summary>
+        /// Delete range of entities from database
+        /// </summary>
         public void DeleteRange<T>(IEnumerable<T> entities) where T : class
         {
             this.DbSet<T>().RemoveRange(entities);
         }
 
+        /// <summary>
+        /// Save changes in database
+        /// </summary>
         public async Task<int> SaveChangesAsync()
         {
             return await this.Context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Disposing context when it is not needed
+        /// </summary>
         public void Dispose()
         {
             this.Context.Dispose();
