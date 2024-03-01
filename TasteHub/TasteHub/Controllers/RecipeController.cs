41e7ac7d0903aacb21c9447b571f6ac1811496fb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-01 13:41:51 +0000

    Created recipe controller with get all recipes method

---
+++ TasteHub/TasteHub/Controllers/RecipeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TasteHub.Core.Contracts;
+using TasteHub.Core.Services;
+
+namespace TasteHub.Controllers
+{
+    [Authorize]
+    public class RecipeController : Controller
+    {
+        private readonly IRecipeService recipeService;
+
+        public RecipeController(IRecipeService _recipeService)
+        {
+            recipeService = _recipeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllRecipes()
+        {
+            var model = await recipeService.GetAllRecipesAsync();
+            return View(model);
+        }
+    }
+}
