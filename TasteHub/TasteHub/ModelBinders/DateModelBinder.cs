5d83f183150f8a26f9af4d7809a35eafb7197f49
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-04 14:34:33 +0000

    Created model binder for date format

---
+++ TasteHub/TasteHub/ModelBinders/DateModelBinder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Data.SqlClient.Server;
+using System.Globalization;
+
+namespace TasteHub.ModelBinders
+{
+    public class DateModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue)) 
+            {
+                DateTime time;
+
+                try
+                {
+                    time = DateTime.ParseExact(valueResult.FirstValue.Trim(), "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None);
+                }
+                catch (Exception ex)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, ex, bindingContext.ModelMetadata);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
