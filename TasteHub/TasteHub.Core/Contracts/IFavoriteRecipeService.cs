91f5b00cd29352a63c8aebc85ebc17d11acf8e49
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-06 16:08:19 +0000

    Created Favorite recipe interface with model

---
+++ TasteHub/TasteHub.Core/Contracts/IFavoriteRecipeService.cs
@@ -0,0 +1,11 @@
+﻿using TasteHub.Core.Models;
+
+namespace TasteHub.Core.Contracts
+{
+    public interface IFavoriteRecipeService
+    {
+        Task<IEnumerable<FavoriteRecipeInfoModel>> GetAllFavoriteRecipesAsync();
+        Task AddAsync();
+        Task RemoveAsync(int id);
+    }
+}
---
+++ TasteHub/TasteHub.Core/Models/FavoriteRecipeInfoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TasteHub.Core.Models
+{
+    public class FavoriteRecipeInfoModel
+    {
+        public FavoriteRecipeInfoModel(
+            string creatorId,
+            string creatorUsername,
+            int recipeId)
+        {
+            CreatorId = creatorId;
+            CreatorUsername = creatorUsername;
+            RecipeId = recipeId;
+        }
+        public string CreatorId { get; set; }
+        public string CreatorUsername { get; set; }
+        public int RecipeId { get; set; }
+    }
+}
