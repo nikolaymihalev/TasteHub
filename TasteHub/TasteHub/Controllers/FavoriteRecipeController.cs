264d4be49ece7075c3497711cff688496e0f38ee
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-07 17:20:13 +0000

    Created favorite recipe controller with my favorite recipes method

---
+++ TasteHub/TasteHub/Controllers/FavoriteRecipeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TasteHub.Core.Contracts;
+
+namespace TasteHub.Controllers
+{
+    public class FavoriteRecipeController : BaseController
+    {
+        private readonly IFavoriteRecipeService favoriteRecipeService;
+
+        public FavoriteRecipeController(IFavoriteRecipeService _favoriteRecipeService)
+        {
+            favoriteRecipeService = _favoriteRecipeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MyFavoriteRecipes()
+        {
+            var allRecipes = await favoriteRecipeService.GetAllFavoriteRecipesAsync();
+            var model = allRecipes.Where(x => x.CreatorId == User.Id());
+
+            return View(model);
+        }
+    }
+}
