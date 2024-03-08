9ac6a59138e9290f56a3c7820170c8bba146a2a0
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-08 18:04:21 +0000

    Add comment service interface

---
+++ TasteHub/TasteHub.Core/Contracts/ICommentService.cs
@@ -0,0 +1,10 @@
+﻿namespace TasteHub.Core.Contracts
+{
+    public interface ICommentService
+    {
+        Task AddSync();
+        Task DeleteAsync();
+        Task GetAllCommentsAboutRecipe();
+        Task GetLastCommentAboutRecipe();
+    }
+}
