6291d3fcb33713e904f2d9546775cfcaf4b44b2e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-08 18:05:35 +0000

    Add and register comment service class

---
+++ TasteHub/TasteHub.Core/Services/CommentService.cs
@@ -0,0 +1,27 @@
+﻿using TasteHub.Core.Contracts;
+
+namespace TasteHub.Core.Services
+{
+    public class CommentService : ICommentService
+    {
+        public Task AddSync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetAllCommentsAboutRecipe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetLastCommentAboutRecipe()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TasteHub/TasteHub/Extensions/ServiceCollectionExtension.cs
+++ TasteHub/TasteHub/Extensions/ServiceCollectionExtension.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<IRecipeService, RecipeService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IFavoriteRecipeService, FavoriteRecipeService>();
+            services.AddScoped<ICommentService, CommentService>();
 
             return services;
         }
