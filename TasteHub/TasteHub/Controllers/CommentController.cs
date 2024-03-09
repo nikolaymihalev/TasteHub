f50a933084bff32a0b62385f306db03743cb245a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-09 12:17:38 +0000

    Created comment controller with get all method

---
+++ TasteHub/TasteHub/Controllers/CommentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TasteHub.Core.Contracts;
+
+namespace TasteHub.Controllers
+{
+    public class CommentController : BaseController
+    {
+        private readonly ICommentService commentService;
+
+        public CommentController(ICommentService _commentService)
+        {
+            commentService = _commentService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAllComments(int id) 
+        {
+            var model = await commentService.GetAllCommentsAboutRecipeAsync(id);
+
+            return View(model);
+        }
+    }
+}
