230dc9d2cf309b2af5932335cbc5c60c4ce2da13
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-10 09:30:09 +0000

    Added interface for rating service

---
+++ TasteHub/TasteHub.Core/Contracts/IRatingService.cs
@@ -0,0 +1,10 @@
+﻿namespace TasteHub.Core.Contracts
+{
+    public interface IRatingService
+    {
+        Task AddAsync();
+        Task DeleteAsync();
+        Task GetAverageRatingAboutRecipeAsync(int recipeId);
+        Task GetAllRatingsAboutRecipeAsync(int recipeId);
+    }
+}
