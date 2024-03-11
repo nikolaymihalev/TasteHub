e6a70ef35cab445118ba8a4898303b934768f2aa
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-11 12:14:06 +0000

    Created rating controller

---
+++ TasteHub/TasteHub/Controllers/RatingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TasteHub.Core.Contracts;
+
+namespace TasteHub.Controllers
+{
+    public class RatingController : BaseController
+    {
+        private readonly IRatingService ratingService;
+
+        public RatingController(IRatingService _ratingService)
+        {
+            ratingService = _ratingService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
