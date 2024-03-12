37da55ec9abf8136dbab6e58d77970a78a0f96fb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-12 14:44:37 +0000

    Created double model binder provider and register it

--- TasteHub/TasteHub/Program.cs
+++ TasteHub/TasteHub/Program.cs
@@ -1,10 +1,14 @@
+using TasteHub.ModelBinders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options => 
+{
+    options.ModelBinderProviders.Insert(0, new DoubleModelBinderProvider());
+});
 
 builder.Services.AddApplicationServices();
 
