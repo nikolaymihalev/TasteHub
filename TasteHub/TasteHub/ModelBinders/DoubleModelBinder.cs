03b32e3d460e5ad16f1c6753a0d02b1228b47d73
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-12 14:42:04 +0000

    Created model binder class for double

---
+++ TasteHub/TasteHub/ModelBinders/DoubleModelBinder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace TasteHub.ModelBinders
+{
+    public class DoubleModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue))
+            {
+                double result = 0;
+                bool success = false;
+
+                try
+                {
+                    string value = valueResult.FirstValue.Trim();
+                    value = value.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    value = value.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    result = double.Parse(value, CultureInfo.CurrentCulture);
+                    success = true;
+                }
+                catch (Exception fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
