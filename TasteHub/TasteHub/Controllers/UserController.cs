93550c32a3b9fcae18bff6b661602215c61562f4
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-17 12:16:00 +0000

    Created user controller

---
+++ TasteHub/TasteHub/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TasteHub.Controllers
+{    
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserController(
+            UserManager<IdentityUser> _userManager,
+            RoleManager<IdentityRole> _roleManager)
+        {
+            userManager = _userManager;
+            roleManager = _roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
