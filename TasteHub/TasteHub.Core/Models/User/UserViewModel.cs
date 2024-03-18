6aa0ce9b473c75a90d0120fa16538186d7eeada7
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-18 17:04:57 +0000

    Create user model

---
+++ TasteHub/TasteHub.Core/Models/User/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TasteHub.Core.Models.User
+{
+    public class UserViewModel
+    {
+        public UserViewModel(
+            string id,
+            string username,
+            int recipesCount)
+        {
+            Id = id;
+            Username = username;
+            RecipesCount = recipesCount;
+        }
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public int RecipesCount { get; set; }
+    }
+}
