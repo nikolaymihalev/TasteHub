aeb696f1d979e5a1a5814f04ac6c9e8d3c536e26
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-19 14:47:08 +0000

    Move all admin operation from user controller to admin controller

---
+++ TasteHub/TasteHub/Controllers/AdminController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TasteHub.Core.Contracts;
+
+namespace TasteHub.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IUserService userService;
+        private readonly IRecipeService recipeService;
+
+        public AdminController(
+            IUserService _userService,
+            IRecipeService _recipeService,
+            RoleManager<IdentityRole> _roleManager,
+            UserManager<IdentityUser> _userManager)
+        {
+            userService = _userService;
+            recipeService = _recipeService;
+            roleManager = _roleManager;
+            userManager = _userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddRole(string roleName)
+        {
+            if (await roleManager.RoleExistsAsync(roleName) == false)
+            {
+                var role = new IdentityRole()
+                {
+                    Name = roleName,
+                };
+
+                await roleManager.CreateAsync(role);
+            }
+
+            return RedirectToAction("AllRecipes", "Recipe");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddUserToRole(string username, string roleName)
+        {
+            if (await roleManager.RoleExistsAsync(roleName))
+            {
+                var user = await userManager.FindByNameAsync(username);
+
+                if (user != null)
+                {
+                    if (await userManager.IsInRoleAsync(user, roleName) == false)
+                    {
+                        await userManager.AddToRoleAsync(user, roleName);
+                    }
+                }
+            }
+
+            return RedirectToAction("AllRecipes", "Recipe");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllUsers()
+        {
+            var model = await userService.GetAllUsersAsync();
+
+            foreach (var user in model)
+            {
+                user.RecipesCount = await recipeService.GetRecipesCountByUsernameAsync(user.Id);
+            }
+
+            return View(model);
+        }
+    }
+}
--- TasteHub/TasteHub/Controllers/UserController.cs
+++ TasteHub/TasteHub/Controllers/UserController.cs
@@ -1,32 +1,21 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using TasteHub.Core.Contracts;
 using TasteHub.Core.Models.User;
 
 namespace TasteHub.Controllers
 {
-    [Authorize]
-    public class UserController : Controller
+    public class UserController : BaseController
     {
         private readonly UserManager<IdentityUser> userManager;
-        private readonly RoleManager<IdentityRole> roleManager;
         private readonly SignInManager<IdentityUser> signInManager;
-        private readonly IUserService userService;
-        private readonly IRecipeService recipeService;
 
         public UserController(
             UserManager<IdentityUser> _userManager,
-            RoleManager<IdentityRole> _roleManager,
-            SignInManager<IdentityUser> _signInManager,
-            IUserService _userService,
-            IRecipeService _recipeService)
+            SignInManager<IdentityUser> _signInManager)
         {
             userManager = _userManager;
-            roleManager = _roleManager;
-            signInManager = _signInManager;
-            userService = _userService;
-            recipeService = _recipeService;
+            signInManager = _signInManager;            
         }
 
         [HttpGet]
@@ -117,58 +106,6 @@ namespace TasteHub.Controllers
             await signInManager.SignOutAsync();
 
             return RedirectToAction("Index", "Home");
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AddRole(string roleName)
-        {
-            if (await roleManager.RoleExistsAsync(roleName) == false)
-            {
-                var role = new IdentityRole()
-                {
-                    Name = roleName,
-                };
-
-                await roleManager.CreateAsync(role);
-            }
-
-            return RedirectToAction("AllRecipes", "Recipe");
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AddUserToRole(string username, string roleName)
-        {
-            if (await roleManager.RoleExistsAsync(roleName))
-            {
-                var user = await userManager.FindByNameAsync(username);
-
-                if (user != null)
-                {
-                    if (await userManager.IsInRoleAsync(user, roleName) == false)
-                    {
-                        await userManager.AddToRoleAsync(user, roleName);
-                    }
-                }
-            }
-
-            return RedirectToAction("AllRecipes", "Recipe");
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> AllUsers()
-        {
-            var model = await userService.GetAllUsersAsync();
-
-            foreach (var user in model) 
-            {
-                int count = await recipeService.GetRecipesCountByUsernameAsync(user.Id);
-                user.RecipesCount = count;
-            }
-
-            return View(model);
-        }
+        }        
     }
 }
