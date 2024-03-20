36bb3d4d25a2de33b47b7d81a633f3a30d727769
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-20 16:16:49 +0000

    Created Views for Become Admin

--- TasteHub/TasteHub.Core/Models/Admin/QueryFormModel.cs
+++ TasteHub/TasteHub.Core/Models/Admin/QueryFormModel.cs
@@ -5,7 +5,7 @@ namespace TasteHub.Core.Models.Admin
 {
     public class QueryFormModel
     {        
-        public string UserId { get; set; }
+        public string UserId { get; set; } = string.Empty;
 
         [Required(ErrorMessage = ErrorMessageConstants.RequireErrorMessage)]
         [StringLength(ValidationConstants.QueryDescriptionMaxLength,
