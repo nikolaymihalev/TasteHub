7a4e365ae37680ecdf41ae25529b3049396103a7
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-25 13:22:56 +0000

    Added summary to Claims extensions

--- TasteHub/TasteHub/Extensions/ClaimsPrincipalExtensions.cs
+++ TasteHub/TasteHub/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,7 +1,14 @@
 ﻿namespace System.Security.Claims
 {
+    /// <summary>
+    /// Extensions for User Claims
+    /// </summary>
     public static class ClaimsPrincipalExtensions
     {
+        /// <summary>
+        /// Get User identifier
+        /// </summary>
+        /// <returns>User identifier</returns>
         public static string Id(this ClaimsPrincipal user)
         {
             return user.FindFirstValue(ClaimTypes.NameIdentifier);
