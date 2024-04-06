4068b9360921aaa507d4a5466826553a06fb1c8b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-04-06 18:44:24 +0000

    Fix search recipe bug

--- TasteHub/TasteHub/wwwroot/js/site.js
+++ TasteHub/TasteHub/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿document.getElementById("searchLink").addEventListener("click", function () {
     var searchTerm = document.getElementById("searchInput").value;
-    var url = "/Recipe/Search?title=" + encodeURIComponent(searchTerm);
+    var url = "/User/Recipe/Search?title=" + encodeURIComponent(searchTerm);
     window.location.href = url; // Redirect to the search URL
 });
